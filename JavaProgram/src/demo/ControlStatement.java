85595f307819faa5b16e77b7abb93048089d56c7
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-13 16:27:38 +0530

    Added control statement

---
+++ JavaProgram/src/demo/ControlStatement.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class ControlStatement {
+
+	public static void main(String[] args) {
+
+		int age = 45;
+		if (age >= 18)
+			System.out.println("You are eligible to give your vote ");
+		else
+			System.out.println("You are not eligible to give vote");
+	}
+
+}
