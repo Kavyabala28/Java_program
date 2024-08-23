21a8d070e7ec6297e0100cf51fd9ed5a78ed95eb
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-23 20:22:34 +0530

    Add files via upload

---
+++ JavaProgram/src/demo/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+		int n1 = 0, n2 = 1, n3;
+
+		int count = 10;
+
+		System.out.println(n1 + " " + n2);
+
+		for (int i = 2; i <= count; i++) {
+			n3 = n1 + n2;
+			System.out.println(n3);
+			n1=n2;
+			n2=n3;
+		}
+
+	}
+
+}
