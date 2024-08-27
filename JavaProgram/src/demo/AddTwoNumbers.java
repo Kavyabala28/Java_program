60030e34d1d8742ac3f89c43d10e26b9a91297c6
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-27 19:55:13 +0530

    Add files via upload

---
+++ JavaProgram/src/demo/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+
+		int number1 = 1000;
+		int number2 = 2000;
+
+		int sum = number1 + number2;
+
+		System.out.println("Addition of " + number1 + " & " + number2 + " is " + sum);
+
+	}
+
+}
