3b19873b474b892a0911eaccc8350b7e0ff07853
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-28 19:08:57 +0530

    Add files via upload

---
+++ JavaProgram/src/demo/ArrayUsingIndex.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class ArrayUsingIndex {
+
+	public static void main(String[] args) {
+
+		String[] name = { "Peacock", "Lion", "Deer", "Tiger" };
+
+		/* Array index starts from 0 */
+
+		System.out.println("Name of 2nd Index : " + name[2]);
+
+	}
+
+}
