27f348589acc4df749da248d0cde1bf3c7075939
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-08-29 12:20:20 +0530

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		
+		/* Array length starts from 1 to n-1 */
+		
+		String[] name = {"Rose", "Jack", "John"};
+		
+		int length = name.length;
+		
+		System.out.println("Length of the array is " +length);
+
+	}
+
+}
