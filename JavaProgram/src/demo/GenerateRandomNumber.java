a7d4ea7fcfe6bb7c9cff748c90b5b436c386cedf
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-10-12 19:32:33 +0530

    Add files via upload

---
+++ JavaProgram/src/demo/GenerateRandomNumber.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class GenerateRandomNumber {
+
+	public static void main(String[] args) {
+		
+		int random = (int) ((Math.random())*99999); // casting
+		System.out.println(random);
+
+	}
+
+}
