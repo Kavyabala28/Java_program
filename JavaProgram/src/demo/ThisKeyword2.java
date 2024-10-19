e9289ffd5e47af291708ff89b342973fac9c0313
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2024-10-19 12:25:35 +0530

    Add files via upload

---
+++ JavaProgram/src/demo/ThisKeyword2.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class ThisKeyword2 {
+
+	public static void main(String[] args) {
+		ThisKeyword s1 = new ThisKeyword(111, "ankit", 5000f);
+		ThisKeyword s2 = new ThisKeyword(112, "sumit", 6000f);
+		s1.display();
+		s2.display();
+
+	}
+
+}
